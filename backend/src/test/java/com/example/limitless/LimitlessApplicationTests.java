2cea933b6a43bbd6bfcca10cf1114cf794706cd6
jasonlawson <jasonlawson301@gmail.com>
2026-08-28 02:31:03 -0400

    Initial backend setup

---
+++ backend/src/test/java/com/example/limitless/LimitlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.limitless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
